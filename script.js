bd5e09fdc0e6303431477606ad9f27186a853124
pyalihtml <bakareren09@gmail.com>
2022-06-21 21:58:53 +0300

    New Update !

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function alertFunction(){
     alert("Yükleniyor... Görevinde Başarılar Dedektif");
 }
+function mFunction(){
+    alert("Lütfen müzikleri aynı anda açmayınız, sadece 1 tanesini açınız. Kodlar düzeltilmeye çalışılacaktır.");
+}
